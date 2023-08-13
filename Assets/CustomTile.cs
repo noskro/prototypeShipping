be6a15f0b63088bd5754027e2417b1b781341a8c
noskro <matthiasluedemann@googlemail.com>
2023-08-13 18:12:10 +0200

    Addes Some Rule Tiles (but by far not all, juts to see how that would work)

--- Assets/CustomTile.cs
+++ Assets/CustomTile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class CustomTile : RuleTile
+public class CustomTile : HexagonalRuleTile
 {
     public EnumTileMovability movability;
 
