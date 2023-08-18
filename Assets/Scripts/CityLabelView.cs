e8059583026918f3a7393b9b3d4a728c4cc510d0
noskro <matthiasluedemann@googlemail.com>
2023-08-18 22:40:59 +0200

    City can have different Sprites depending on City Level

--- Assets/Scripts/CityLabelView.cs
+++ Assets/Scripts/CityLabelView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,9 +8,17 @@ public class CityLabelView: MonoBehaviour
     public TextMeshPro textCityName;
     public TextMeshPro textCityLevel;
 
+    public SpriteRenderer CitySprite;
+    public List<Sprite> SpriteCityLevelList;
+
     internal void SetCityData(PersistentCityData persistentCityData)
     {
         textCityName.text = persistentCityData.CityName;
         //textCityLevel.text = persistentCityData.CityLevel.ToString();
+
+        if (persistentCityData.CityLevel >= 0 && persistentCityData.CityLevel < SpriteCityLevelList.Count)
+        {
+            CitySprite.sprite = SpriteCityLevelList[persistentCityData.CityLevel];
+        }
     }
 }
