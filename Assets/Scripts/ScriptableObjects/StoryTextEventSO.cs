e8b5b789e93d19ef2ca6b4e87a42cadb65b271f7
bitmedic <bitmedic01@gmail.com>
2023-08-27 08:50:42 +0200

    Show story text as text boxes in inspector

--- Assets/Scripts/ScriptableObjects/StoryTextEventSO.cs
+++ Assets/Scripts/ScriptableObjects/StoryTextEventSO.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class StoryTextEventSO: ScriptableObject
 {
     public string Title;
+    [TextArea(5, 20)]
     public List<string> Texts;
 
     public List<StoryTextEventTrigger> StoryTextEventTrigger;
